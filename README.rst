eba838ca6aaa4b8ceb31f45becbe27ec73801698
Vishal Soni <vishal3soni@gmail.com>
2018-05-10 09:59:40 +0530

    Packaging

---
+++ README.rst
@@ -0,0 +1,15 @@
+============
+Sticky Notes
+============
+Sticky notes for your desktop
+
+Features
+========
+*Colorful Text
+*Colorful Background
+*Font Increase/Decrease
+
+Usage
+=====
+from terminal: initsticky
+To run in background: initsticky & OR nohup initsticky &
--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='Sticky',
       version='0.1.0',
       description='Sticky Notes',
       url='http://github.com/veeshalsoni/sticky',
       author='Vishal Soni',
       author_email='vishal3soni@gmail.com',
+      keywords='StickyNotes Sticky Notes',
       license='GPLv3',
+      long_description=readme(),
+      classifiers=[
+	    'Development Status :: 4 - Beta',
+		'Programming Language :: Python',
+    	'Intended Audience :: General',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+	  ],
+	  pacage_data=,
       install_requires=['PySide'],
       scripts=["initsticky"],
       packages=['sticky'])
